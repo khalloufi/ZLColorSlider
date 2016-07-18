3af3b425b251cf9bf516df237fb8054b3c7ce8ed
khalloufi <issam.khalloufi@gmail.com>
2016-07-18 11:19:02 +0100

    Version 1.0

--- ZLColorSlider/ZLColorSlider/Classes/ZLColorSlider.swift
+++ ZLColorSlider/ZLColorSlider/Classes/ZLColorSlider.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ZLColorSlider: UISlider {
-    var indicatorOffset:CGFloat = -40
+    var indicatorOffset:CGFloat = -25
     var showIndicator:Bool = true
     var colorTrackHeight:Int = 2
     var indicatorApprearAnimationDuration:NSTimeInterval = 0.07
@@ -36,13 +36,15 @@ class ZLColorSlider: UISlider {
         initialize()
     }
     func initialize() {
-        let image:UIImage = UIImage(named: "colorTrack.png")!
+        let image:UIImage = UIImage(named: "slider-color.png")!
         colorTrackImageView = UIImageView(image: image)
         addSubview(colorTrackImageView)
         sendSubviewToBack(colorTrackImageView)
         
         setMinimumTrackImage(imageFromColor(UIColor.clearColor()), forState: UIControlState.Normal)
         setMaximumTrackImage(imageFromColor(UIColor.clearColor()), forState: UIControlState.Normal)
+        
+        self.thumbTintColor = UIColor(hue: 0.5,saturation: 1.0,brightness: 1.0,alpha: 1.0)
         
         indicatorView = nil
     }
@@ -54,7 +56,6 @@ class ZLColorSlider: UISlider {
         rect.size.height = CGFloat(colorTrackHeight)
         colorTrackImageView.frame = rect
         colorTrackImageView.center = center
-        self.thumbTintColor = UIColor(hue: CGFloat(self.value),saturation: 1.0,brightness: 1.0,alpha: 1.0)
     }
 
     override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
@@ -68,6 +69,7 @@ class ZLColorSlider: UISlider {
                     addSubview(indicatorView)
                     UIView.animateWithDuration(indicatorApprearAnimationDuration, animations: {
                         self.indicatorView.alpha = 1
+                        self.thumbTintColor = UIColor.clearColor()
                     })
                 }
             }
@@ -90,7 +92,7 @@ class ZLColorSlider: UISlider {
                     rect.origin.x = CGRectGetMidX(currentThumbRect()) - CGRectGetWidth(rect)/2;
                     indicatorView.frame = rect
                     indicatorView.color = UIColor(hue: CGFloat(self.value),saturation: 1.0,brightness: 1.0,alpha: 1.0)
-                    self.thumbTintColor = UIColor(hue: CGFloat(self.value),saturation: 1.0,brightness: 1.0,alpha: 1.0)
+                    self.thumbTintColor = UIColor.clearColor()
                 }
             }
         }
@@ -100,6 +102,7 @@ class ZLColorSlider: UISlider {
         super.endTrackingWithTouch(touch, withEvent: event)
         if showIndicator{
             removeIndicator()
+            self.thumbTintColor = UIColor(hue: CGFloat(self.value),saturation: 1.0,brightness: 1.0,alpha: 1.0)
         }
     }
     override func cancelTrackingWithEvent(event: UIEvent?) {
@@ -144,6 +147,15 @@ class ZLColorSlider: UISlider {
         let image:UIImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         return image
+    }
+    func imageWithImage(image: UIImage, scaledToSize newSize: CGSize) -> UIImage
+    {
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0)
+        image.drawInRect(CGRectMake(0, 0, newSize.width, newSize.height))
         
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage;
     }
 }
